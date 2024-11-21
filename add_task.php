96048883b0dc50eb97a69561487ada40483cb10e
Marjory-2003 <marjoryluyong479@gmail.com>
2024-11-21 09:10:20 +0800

    add_task.php

--- add_task.php
+++ add_task.php
@@ -5,6 +5,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $task = $_POST['task'];
     $sql = "INSERT INTO tasks (task) VALUES ('$task')";
     $conn->query($sql);
-    header("Location: indexs.php");
+    header("Location: index.php");
 }
 ?>
